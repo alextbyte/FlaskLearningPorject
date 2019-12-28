5711bdf5a16f411c9a24c4ab98eb1e648d545ea7
Alex <alexandrbuk@hotmail.com>
2019-12-28 03:16:20 +0000

    Commit 28-12-2019: Flask-RESTful Api developed with Json data format

--- Desktop/CODE/PyFlask/ConferenceBarrel/main.py
+++ Desktop/CODE/PyFlask/ConferenceBarrel/main.py
@@ -11,9 +11,42 @@
 By default we need lways to restart server to see the changes,
 but by setting «debug» variable we can change that: 
                     $Env:FLASK_DEBUG=1
+
 After that all the chages will be seen after refreshing html page
 
+-----------------------------------------
+Flask Quick Guide:
+
+=> To create new html page /OR/ «view»:
+(«view» includes wraper «@app.route('pages')» + function «def page():»)
+1. Create new html in the template
+2. Create new route: @app.route('/new_page')
+3. Create a function name same to wrap: def new_page():
+
+=> To pass variable into html page
+1. Pass first into: @app.roure('/newpage/<var_type:Variable')
+2. Pass variable into function: def new_page(Variable):
+3. Pass into html: render_template('new_page.html', variable_html=Variable)
+
+=> To pass variable from the funtion into other html page if «redirect» is used:
+1. Pass varriable into «redirect»: redirect(url_for('new page', Variable=var_name))
+2. Than use "To pass variable into html page" instrutions
+
+=> To create sub-pages wrap sub-page into main page
+1. Wrap into main page: @app.route('/main_page')
+2. Wrap into sub-page: @app.route('/main_page/sub_page')
+3. Create a function with same name as sub-page: def sub_page():
+
+=> To protect html page with login seccion
+(Only can be accessed if login seccion id opened)
+1. Wrap into html page: @app.route('/html_page')
+2. Wrap into login wrap: @login_required
+3. Create the function of thml page: def html_apges(): 
+
+
 '''
+
+
 # Import Flask application object
 # reneder_template() -> to render html page inside the «basetemplatepage.html»
 # abrot() -> to display «404» error if page does not exists
@@ -23,11 +56,15 @@ from flask import Flask, render_template, abort, request, redirect, url_for, fla
 
 # Library for user login control
 # To import it you need to install it -> pip install flask_login
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
 
 # Import SQLAlchemy to manage DB
 from flask_sqlalchemy import SQLAlchemy
+
+
+# Importing Flask API and Resource
+from flask_restful import Api, Resource 
 
 
 # Date time for db use
@@ -41,7 +78,10 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-
+# Flask Restful API initializer
+# assotiate url rules with resources
+# the resource will handle the http request ad send back response in Json
+api = Api(app)
 
 ####### Setting up the data base
 # We will use Flask-SQLAlchemy to write code and test on SQLite
@@ -68,7 +108,28 @@ login_manager.init_app(app)
 # To redirect page to login when user is logout
 login_manager.login_view = 'login'
 
+#################################
+#           DB MODELS           #
 
+
+# DB for Conferences
+# Create a class to manage Conference DB:
+class Conference(db.Model):
+    #### this vars: «id», «title»,«date»,«ticket_cost» => will be used in the html page inside the « block » : <h3> Conference name: {{conference_details.title}} </h3>
+    # field name = database.Column(value/field type, constrains «primary_key=True», «nullable=False» (NOT NULL), «default=datetime.utcnow»(if an object is not assigned by defaut it will assume todays date)) 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ticket_cost = db.Column(db.Float, nullable=False)
+    # Establish Manay to Many relationship to «ConferenceBarrelUser» through «ConferenceBarrelTickets» table
+    attendees = db.relationship('ConferenceBarrelUser', secondary='conference_barrel_tickets')
+
+    # Function for string representation of the method
+    def __repr__(self):
+        return f'Conference {self.title}'
+
+
+# DB for users
 # Setting up the class for used data
 # User Class will inherit UserMixin class
 # For thsi case use defualt configurations
@@ -78,6 +139,8 @@ class ConferenceBarrelUser(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128), nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    # Establish Manay to Many relationship to «Conference» through «ConferenceBarrelTickets» table
+    conferences = db.relationship('Conference', secondary='conference_barrel_tickets')
 
     def __init__(self, username, password):
         self.username = username
@@ -89,23 +152,41 @@ class ConferenceBarrelUser(db.Model, UserMixin):
 # create data base by «db.create_all()»
 
 
-
+# DB for relating two columns «Conference» and «ConferenceBarrelUsers»
+# This is intermedia table in Many to Many relation
 # Create a class to manage Conference DB:
-class Conference(db.Model):
+class ConferenceBarrelTickets(db.Model):
     #### this vars: «id», «title»,«date»,«ticket_cost» => will be used in the html page inside the « block » : <h3> Conference name: {{conference_details.title}} </h3>
     # field name = database.Column(value/field type, constrains «primary_key=True», «nullable=False» (NOT NULL), «default=datetime.utcnow»(if an object is not assigned by defaut it will assume todays date)) 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    ticket_cost = db.Column(db.Float, nullable=False)
+    # To reference this collumn to other use «name_of_the_table.id»
+    # Every name of the tabe is converted «TableName» -> into -> «table_name»
+    user_id = db.Column(db.Integer, db.ForeignKey('conference_barrel_user.id'))
+    conference_id = db.Column(db.Integer, db.ForeignKey('conference.id'))
+    # We would use to generate code to access related into in other tables by «user» and «conference» «id» 
+    # Hoever with SQLAlchemy we can reference class models directly to acess particular info fro the table without referencing «id»
+    # var related to db «ConferenceBarrelUser» with back reference to filed «ticket», and with «lazy='dynamic'» not load untiil data is accessed
+    # for thsi relationship work we need to add same relations into Model classess related here: «ConferenceBarreslUser» and «Conference»
+    user = db.relationship(ConferenceBarrelUser, backref=db.backref('tickets', lazy='dynamic'))
+    conference = db.relationship(Conference, backref=db.backref('tickets', lazy='dynamic'))
 
-    # Function for string representation of the method
-    def __repr__(self):
-        return f'Conference {self.title}'
+# After creatign intermedia table «ConferenceBarrelTickets» between «Conference» and «ConferenceBarrelUser» table
+# Update the the intermedia table «ConferenceBarrelTickets» through the ipython or in thr way
+
+#### All 3 tabes are contained inside «conferencebarrel.sqlite» db file -> was created manualy
 
 
-# After create class «Conference» -> VIEW FILE CALLED «manage_db_in_terminal_info»
 
+'''
+# Inserting data into DBs: 
+After creation of class MODEL «Conference» or any other class MODEL
+ -> VIEW FILE CALLED «manage_db_in_terminal_info»
+ for How to insert data into data base
+
+'''
+
+#           DB MODELS           #
+#################################
 
 
 
@@ -157,6 +238,7 @@ def conferences(conference_id=None):
         # If «conference_details» object is None display «404» page error
         if conference_details is None:
             abort(404)
+        # «conference_details» - is var to pass all conference info
         return render_template('details.html', conference_details=conference_details)
     else:
         # To view data base just use variable to pass data base values: conferences = Conference.query.all()
@@ -218,11 +300,11 @@ def login():
 
 # Creating protected resource
 # Passing «<username>» variable into the wraper to pass it into the function
-@app.route('/secret_data/<username>')
+@app.route('/secret_data')
 @login_required # To protect this page from unauthorized access
-def secret_data(username):
+def secret_data():
     # Formating varible appearence: username=str(username).capitalize()+' ('+username+')'
-    return render_template('secret_data.html', username=username)
+    return render_template('secret_data.html')
 
 # create a logout function
 @app.route('/logout')
@@ -248,6 +330,15 @@ def register():
 
         # Check if username does already exist in the data base
         usrname = ConferenceBarrelUser.query.filter(ConferenceBarrelUser.username == username).first()
+
+
+        # Verify if username was typed into form field
+        if username == '':
+            # Show flash message explainig what happend
+            flash("Username can't empty!")
+            # return back to register page
+            return redirect(url_for('register'))
+
 
         # if username is NOT NONE return register page again
         if usrname is not None:
@@ -279,3 +370,64 @@ def register():
     else:
         # If method used is not 'POST' redirect to «register.html»
         return render_template('register.html')
+
+
+# To create a new ticket
+# - and than redirect user to a «prefile» view/html page
+@app.route('/new_ticket/<int:conference_id>')
+@login_required
+def new_ticket(conference_id):
+    # Before querying the «conference_id» it should be add ErrorHandling
+    # - but it will be omited in here
+    conference = Conference.query.get(conference_id)
+    ticket = ConferenceBarrelTickets(user=current_user, conference=conference)
+    # Insert inserted data into data base
+    # we need to check for a duplicates, but not in this demo
+    db.session.add(ticket) # add to the session
+    db.session.commit() # write into db -> make the changes
+    return redirect(url_for('profile'))
+
+
+# Personal user profile:
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
+
+
+
+# Resource to retrive the list of conferences
+# The resource class has a methods for each http methods: «GET», «PUT», «POST»
+class ConferenceResource(Resource):
+    # to retrive a list of conferences
+    # «def get(self)» will handle http get request
+    # Passed variable («conference_id») has to have value
+    def get(self, conference_id=None):
+        # Return value is a list of dictionaries: one dictionary for each «Conference Model» in the data base
+        # Flask-RESTful wil strigify the content and return pure Json
+        # To re-use the resource to give the details of the single conference
+        ## If «conference_id» is None retrieve all conferences «id» and «title»
+        if conference_id is None:
+            return [ 
+                {
+                    'id': conference.id,
+                    'title': conference.title
+                }
+                # loop through the conferences
+                for conference in Conference.query.all()
+            ]
+        # «Else» (if there are conference id), query all caonferences in data base
+        # - and retrieve conference details 
+        else:
+            conference = Conference.query.get(conference_id)
+            return {
+                'id': conference.id,
+                'title': conference.title,
+                'date': conference.date.strftime('%m/%d/%Y'),
+                'ticket_cost': conference.ticket_cost,
+                'attendee_count': len(Conference.query.all())
+            }
+
+
+# To associate «Resource» with «url rule» the API instace provides the «Add resource» method
+api.add_resource(ConferenceResource, '/api/conferences', '/api/conferences/<int:conference_id>')
